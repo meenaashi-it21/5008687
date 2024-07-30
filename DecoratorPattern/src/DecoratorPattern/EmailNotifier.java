f76e77fb8703d119c3c2020c7853e5fffe7e7d9c
MEENAASHI SIVASUBRAMANI <95066759+meenaashi-it21@users.noreply.github.com>
2024-07-30 07:15:42 +0530

    Delete DecoratorPattern directory

--- DecoratorPattern/src/DecoratorPattern/EmailNotifier.java
+++
@@ -1,7 +0,0 @@
-package DecoratorPattern;
-
-public class EmailNotifier implements Notifier {
-	 public void send(String message) {
-	        System.out.println("Sending email notification: " + message);
-	    }
-}
